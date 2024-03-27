575201b1ad17c01819a89beda50dfcbeba185c0e
hnegi2531 <himanshu.dev7350@gmail.com>
2024-03-28 01:18:10 +0530

    swap page added
    swap page image issue fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ const navLinks: NavLinkType[] = [
   { id: 1, text: "earn", route: "/earn", isPrivateRoute: true },
   { id: 2, text: "score", route: "/score", isPrivateRoute: true },
   { id: 3, text: "leaderboard", route: "/leaderboard", isPrivateRoute: false },
-  { id: 4, text: "discord", route: "https://discord.gg/PG8w4RG5jJ", isPrivateRoute: false },
+  { id: 4, text: "swap", route: "/swap", isPrivateRoute: false },
 ];
 
 const Navbar = () => {
@@ -46,21 +46,13 @@ const Navbar = () => {
           {navLinks.map((link) => {
             const showLink = (link.isPrivateRoute && isUserLoggedIn) || !link.isPrivateRoute;
             const isActive = currentRoute === link.route;
-            return link.id === 4 ? (
-              <a
-                href={link.route}
-                target="_blank"
-                className={`uppercase text-base hover:text-brand-300 transition-all duration-100 pb-1 select-none ${isActive ? "border-b text-brand-300 border-brand-300" : "text-white"
-                  } ${showLink ? "flex" : "hidden"}`}
-              >
-                {link.text}
-              </a>
-            ) : (
+            return (
               <NavRoute
                 key={link.id}
                 route={link.route}
-                className={`text-base hover:text-brand-300 transition-all duration-100 pb-1 select-none ${isActive ? "border-b text-brand-300 border-brand-300" : "text-white"
-                  } ${showLink ? "flex" : "hidden"}`}
+                className={`text-base hover:text-brand-300 transition-all duration-100 pb-1 select-none ${
+                  isActive ? "border-b text-brand-300 border-brand-300" : "text-white"
+                } ${showLink ? "flex" : "hidden"}`}
               >
                 {link.text}
               </NavRoute>
